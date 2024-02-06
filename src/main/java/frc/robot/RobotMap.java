b472106cbe5d67362c13286a666156de2fb075cd
David Cho <63265252+davidch2020@users.noreply.github.com>
2024-02-06 17:04:37 -0500

    Update RobotMap.java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,4 +2,6 @@ package frc.robot;
 
 public class RobotMap {
     public static final int INTAKE_MOTOR = 0; 
+    public static final int IR_SENSOR_1 = 0; 
+    public static final int IR_SENSOR_2 = 0;
 }
