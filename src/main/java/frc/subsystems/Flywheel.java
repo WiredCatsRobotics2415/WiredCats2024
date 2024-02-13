120365912473dec7ebb1566d2042214f7df91c39
David Cho <63265252+davidch2020@users.noreply.github.com>
2024-02-13 17:16:12 -0500

    Changes

--- src/main/java/frc/subsystems/Flywheel.java
+++ src/main/java/frc/subsystems/Flywheel.java
@@ -55,8 +55,6 @@ public class Flywheel extends SubsystemBase {
         for (int i = 0; i < 5; ++i) {
             status = left.getConfigurator().apply(configs);
             if (status.isOK()) break;
-            status = left.getConfigurator().apply(configs);
-            if (status.isOK()) break;
         }
         if(!status.isOK()) {
         System.out.println("Could not apply configs, error code:] " + status.toString());
