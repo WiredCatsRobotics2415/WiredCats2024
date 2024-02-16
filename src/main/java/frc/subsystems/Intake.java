f1741ba68b89bedcf35a9297f6b00f46bf21102d
Snapwhiz914 <asilversind@gmail.com>
2024-02-16 07:55:44 -0500

    Organize intake.java

--- src/main/java/frc/subsystems/Intake.java
+++ src/main/java/frc/subsystems/Intake.java
@@ -1,91 +1,85 @@
 package frc.subsystems;
+
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import frc.robot.Constants;
 import frc.robot.RobotMap;
 import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-    private TalonFX motor;
-    private static Intake instance;
-    private boolean state;
-    private AnalogInput topIR;
-    private AnalogInput bottomIR;
+  private TalonFX motor;
+  private static Intake instance;
+  private boolean state;
+  private AnalogInput topIR;
+  private AnalogInput bottomIR;
 
-    public Intake() {
-        //motor = new CANSparkMax(RobotMap.Intake.INTAKE_MOTOR, CANSparkMax.MotorType.kBrushless);
-        motor = new TalonFX(0);
-        topIR = new AnalogInput(RobotMap.Intake.TOP_IR);
-        bottomIR = new AnalogInput(RobotMap.Intake.BOTTOM_IR); 
-        state = false;
-    }
-
-    public Command off() {
-      return runOnce(
-        () -> {
-      motor_off();
-        });
-    }
-
-    public Command out() {
-       return runOnce(
-        () -> {
-      motor.set(Constants.Intake.IntakeSpeed);
-        });
-    }
-
-    public void motor_in() {
-        System.out.println("in");
-        motor.set(-Constants.Intake.IntakeSpeed);
-    }
-
-    public void motor_off() {
-      System.out.println("off");
-      motor.set(0);
+  public Intake() {
+    // motor = new CANSparkMax(RobotMap.Intake.INTAKE_MOTOR,
+    // CANSparkMax.MotorType.kBrushless);
+    motor = new TalonFX(0);
+    topIR = new AnalogInput(RobotMap.Intake.TOP_IR);
+    bottomIR = new AnalogInput(RobotMap.Intake.BOTTOM_IR);
+    state = false;
   }
 
-  public void motor_out() {
-    System.out.println("out");
-    motor.set(Constants.Intake.IntakeSpeed);
-}
+  public static Intake getInstance() {
+    if (instance == null) {
+      instance = new Intake();
+    }
+    return instance;
+  }
 
-    public static Intake getInstance() {
-        if (instance == null) {
-          instance = new Intake();
-        }
-        return instance;
-      }
+  //COMMANDS
+  public Command off() {
+    return runOnce(() -> motorOff());
+  }
 
-      public Command toggle() {
-        return runOnce(
+  public Command out() {
+    return runOnce(() -> motorOut());
+  }
+
+  public Command in() {
+    return runOnce(() -> motorIn());
+  }
+
+  public Command toggle() {
+    return runOnce(
         () -> {
           System.out.println("Run");
           if (state == true) {
-            motor_off();
+            motorOff();
             state = false;
-        } else {
-            motor_in();
+          } else {
+            motorIn();
             state = true;
-        }
-      });
-      }
-        
-
-      public boolean hasNote() {
-          return topIR.getValue() > Constants.Intake.IRThreshold;
-        }
-
-      public boolean inShooter() {
-        return bottomIR.getValue() > Constants.Intake.IRThreshold;
-      }
-
-      public Command intakeIn() {
-        return runOnce(
-        () -> {
-        motor_in();
+          }
         });
-      }
+  }
+
+  //MOTOR METHODS
+  public void motorIn() {
+    System.out.println("in");
+    motor.set(-Constants.Intake.IntakeSpeed);
+  }
+
+  public void motorOff() {
+    System.out.println("off");
+    motor.set(0);
+  }
+
+  public void motorOut() {
+    System.out.println("out");
+    motor.set(Constants.Intake.IntakeSpeed);
+  }
+
+  //SENSOR METHODS
+  public boolean hasNote() {
+    return topIR.getValue() > Constants.Intake.IRThreshold;
+  }
+
+  public boolean inShooter() {
+    return bottomIR.getValue() > Constants.Intake.IRThreshold;
+  }
 }
