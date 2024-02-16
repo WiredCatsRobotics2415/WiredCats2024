b5c58e9f9b1ffd61ba6a05a1d9f6a4ddc1030897
David Cho <63265252+davidch2020@users.noreply.github.com>
2024-02-16 15:20:44 -0500

    target closest

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.commands.FixAll;
 import frc.commands.HotspotGeneator;
 import frc.generated.TunerConstants;
