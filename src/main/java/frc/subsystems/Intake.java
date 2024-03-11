000526e9b22af5463b5305f81d914891f55f7005
Snapwhiz914 <asilversind@gmail.com>
2024-03-11 08:21:19 -0400

    Formatted & corrected intake commands

--- src/main/java/frc/subsystems/Intake.java
+++ src/main/java/frc/subsystems/Intake.java
@@ -1,13 +1,8 @@
 package frc.subsystems;
 
-import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
 
-import frc.robot.Constants;
-import frc.robot.Robot;
-import frc.robot.RobotMap;
-import frc.subsystems.Flywheel;
-import frc.sim.PhysicsSim;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
@@ -18,204 +13,227 @@ import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+import frc.sim.PhysicsSim;
+import frc.utils.Logger;
+import frc.utils.Logger.LogLevel;
+
 public class Intake extends SubsystemBase {
-  private TalonFX motor;
-  private static Intake instance;
-  private AnalogInput closeToFlywheelSensor;
-  private AnalogInput closeToIntakeSensor;
-  private PositionVoltage positionOut = new PositionVoltage(0, 0, false, 0, 0, false, false, false); 
-  
-  private final Flywheel flywheel = Flywheel.getInstance();
+    private TalonFX motor;
+    private static Intake instance;
+    private AnalogInput closeToFlywheelSensor;
+    private AnalogInput closeToIntakeSensor;
+    private PositionVoltage positionOut =
+            new PositionVoltage(0, 0, false, 0, 0, false, false, false);
 
-  private boolean state = false;
+    private final Flywheel flywheel = Flywheel.getInstance();
 
-  private MechanismLigament2d speed;
+    private MechanismLigament2d speed;
 
-  private double uptakeSpeed = Constants.Intake.UptakeSpeed;
-  private double intakeSpeed = Constants.Intake.IntakeSpeed;
-  private double outtakeSpeed = Constants.Intake.OuttakeSpeed;
+    private double uptakeSpeed = Constants.Intake.UptakeSpeed;
+    private double intakeSpeed = Constants.Intake.IntakeSpeed;
+    private double outtakeSpeed = Constants.Intake.OuttakeSpeed;
 
-  public boolean isBeingIntook = false;
-  public boolean isBeingQueued = false;
+    public boolean isBeingIntook = false;
+    public boolean isBeingQueued = false;
+    private boolean state = false;
 
-  public Intake() {
-    motor = new TalonFX(RobotMap.Intake.INTAKE_MOTOR);
-    motor.optimizeBusUtilization();
-    motor.setInverted(true);
-    state = false;
+    public Intake() {
+        motor = new TalonFX(RobotMap.Intake.INTAKE_MOTOR);
+        motor.optimizeBusUtilization();
+        motor.setInverted(true);
 
-    SmartDashboard.setDefaultNumber("Uptake", uptakeSpeed);
-    SmartDashboard.setDefaultNumber("Intake", intakeSpeed);
-    SmartDashboard.setDefaultNumber("Outtake", outtakeSpeed);
+        closeToFlywheelSensor = new AnalogInput(RobotMap.Intake.FLYWHEEL_IR);
+        closeToIntakeSensor = new AnalogInput(RobotMap.Intake.INTAKE_IR);
 
-    Mechanism2d intakeMech2d = new Mechanism2d(3, 3);
+        configureSmartDashboardWidgets();
 
-    MechanismRoot2d root = intakeMech2d.getRoot("intake", 1.2, 0.25);
-    speed = root.append(new MechanismLigament2d("intake", 0.1, 0));
-
-    Shuffleboard.getTab("Mechanism2d").add("Intake Mechanism", intakeMech2d);
-
-    if (Robot.isSimulation()) {
-      PhysicsSim.getInstance().addTalonFX(motor, 0.001);
-    }
-
-    closeToFlywheelSensor = new AnalogInput(RobotMap.Intake.FLYWHEEL_IR);
-    closeToIntakeSensor = new AnalogInput(RobotMap.Intake.INTAKE_IR);
-  }
-
-  /**
-   * Sets the motor's speed to the IntakeSpeed.
-   */
-  public void motorIn() {
-    System.out.println("in");
-    motor.set(SmartDashboard.getNumber("Intake", intakeSpeed));
-    speed.setColor(new Color8Bit(0, (int) (intakeSpeed * 255), 0));
-  }
-
-  /**
-   * Sets the motor's speed to the UptakeSpeed.
-   */
-  public void motorUptake() {
-    System.out.println("uptake");
-    motor.set(SmartDashboard.getNumber("Uptake", uptakeSpeed));
-    speed.setColor(new Color8Bit(0, (int) (uptakeSpeed * 255), 0));
-  }
-
-  /**
-   * Sets the motor's speed to 0. Note that the motor is not configured to be in
-   * brake
-   * or coast mode in this subsystem.
-   */
-  public void motorOff() {
-    System.out.println("off");
-    motor.set(0);
-    speed.setColor(new Color8Bit(0, 0, 0));
-  }
-
-  /**
-   * Sets the motor's speed to the OuttakeSpeed
-   */
-  public void motorOut() {
-    System.out.println("out");
-    motor.set(SmartDashboard.getNumber("Outtake", outtakeSpeed));
-    speed.setColor(new Color8Bit(0, 0, (int) (outtakeSpeed * 255)));
-  }
-
-  public static Intake getInstance() {
-    if (instance == null) {
-      instance = new Intake();
-    }
-    return instance;
-  }
-
-  // COMMANDS
-  /**
-   * @return Command that sets the motor speed to 0.
-   */
-  public Command off() {
-    isBeingIntook = false;
-    isBeingQueued = false;
-    return runOnce(() -> motorOff());
-  }
-
-  /**
-   * @return Command that sets the motor speed to OuttakeSpeed.
-   */
-  public Command out() {
-    isBeingIntook = false;
-    isBeingQueued = false;
-    return runOnce(() -> motorOut());
-  }
-
-  /*public Command auto() {
-    while (ir.rightIR.getValue() < Constants.Intake.IRThreshold) {
-      return runOnce(() -> motorIn());
-    }
-    return runOnce(() -> motorOff());
-  }*/
-
-  /**
-   * @return Command that sets the motor speed to IntakeSpeed.
-   */
-  public Command in() {
-    isBeingIntook = true;
-    isBeingQueued = false;
-    return runOnce(() -> motorIn());
-  }
-
-  public Command queueNote(){ //outake note slighly
-    isBeingIntook = false;
-    isBeingQueued = true;
-    return runOnce(() -> motor.set(-0.05));
-  }
-
-  public Command stopNoteForShooting(){
-    isBeingIntook = false;
-    isBeingQueued = false;
-
-    flywheel.on(500,500).schedule();
-
-    return runOnce(() -> motorOff());
-  }
-
-  /**
-   * @return Command that sets the motor speed to UptakeSpeed.
-   */
-  public Command uptake() {
-    return runOnce(() -> motorUptake());
-  }
-
-  /**
-   * @return Command that toggles between intaking and not intaking.
-   *         Does NOT take into account other motor modes, ie. If this has been
-   *         called,
-   *         and the out Command is run, then when this command is run again, the
-   *         motor
-   *         will just be turned off.
-   */
-  public Command toggleIntake() {
-    return runOnce(
-        () -> {
-          if (state == true) {
-            motorOff();
-            state = false;
-          } else {
-            motorIn();
-            state = true;
+        if (Robot.isSimulation()) {
+            PhysicsSim.getInstance().addTalonFX(motor, 0.001);
         }
-      });
     }
-  
-  @Override
-  public void periodic() {
-    // SmartDashboard.putNumber("IR", leftIR.getValue()); 
-  }
 
-  /**
-   * @return true if the note has been intook (used to signal intake off)
-   */
-  public boolean hasNote() {
-      //return ((ir.rightIR.getValue() > Constants.Intake.IRThreshold) || (ir.leftIR.getValue() > Constants.Intake.IRThreshold)) && isBeingIntook;
-      return closeToFlywheelSensor.getValue() > Constants.Intake.IRThreshold;
-  }
+    public static Intake getInstance() {
+        if (instance == null) {
+            instance = new Intake();
+        }
+        return instance;
+    }
 
-  /**
-   * @return true if note is queued for shooting (clear from flywheel)
-   */
-  public boolean noteIsQueued() {
-    return closeToFlywheelSensor.getValue() < Constants.Intake.IRThreshold;
-    //return ((ir.rightIR.getValue() < Constants.Intake.IRThreshold) && (ir.leftIR.getValue() < Constants.Intake.IRThreshold)) && isBeingQueued;
-  }
+    private void configureSmartDashboardWidgets() {
+        SmartDashboard.setDefaultNumber("Uptake", uptakeSpeed);
+        SmartDashboard.setDefaultNumber("Intake", intakeSpeed);
+        SmartDashboard.setDefaultNumber("Outtake", outtakeSpeed);
 
-  public boolean isBeingIntook(){
-    return isBeingIntook;
-  }
+        Mechanism2d intakeMech2d = new Mechanism2d(3, 3);
 
-  public boolean isBeingQueued(){
-    return isBeingQueued;
-  }
+        MechanismRoot2d root = intakeMech2d.getRoot("intake", 1.2, 0.25);
+        speed = root.append(new MechanismLigament2d("intake", 0.1, 0));
 
-  public void motorInWithRotations(double rotations){ 
-    motor.setControl(positionOut.withPosition(rotations)); //Not 100% Sure PositionOut uses rotations 
-  }
+        Shuffleboard.getTab("Mechanism2d").add("Intake Mechanism", intakeMech2d);
+    }
+
+    /** Sets the motor's speed to the IntakeSpeed. */
+    private void motorIn() {
+        Logger.log(this, LogLevel.INFO, "motor in at " + intakeSpeed);
+        motor.set(SmartDashboard.getNumber("Intake", intakeSpeed));
+        speed.setColor(new Color8Bit(0, (int) (intakeSpeed * 255), 0));
+    }
+
+    /** Sets the motor's speed to the UptakeSpeed. */
+    private void motorUptake() {
+        Logger.log(this, LogLevel.INFO, "motor uptaking at " + uptakeSpeed);
+        motor.set(SmartDashboard.getNumber("Uptake", uptakeSpeed));
+        speed.setColor(new Color8Bit(0, (int) (uptakeSpeed * 255), 0));
+    }
+
+    /**
+     * Sets the motor's speed to 0. Note that the motor is not configured to be in brake or coast
+     * mode in this subsystem.
+     */
+    private void motorOff() {
+        Logger.log(this, LogLevel.INFO, "motor off");
+        motor.set(0);
+        speed.setColor(new Color8Bit(0, 0, 0));
+    }
+
+    /** Sets the motor's speed to the OuttakeSpeed */
+    private void motorOut() {
+        Logger.log(this, LogLevel.INFO, "motor outtaking at " + outtakeSpeed);
+        motor.set(SmartDashboard.getNumber("Outtake", outtakeSpeed));
+        speed.setColor(new Color8Bit(0, 0, (int) (outtakeSpeed * 255)));
+    }
+
+    // COMMANDS
+    /**
+     * @return Command that sets the motor speed to 0.
+     */
+    public Command off() {
+        return runOnce(
+                () -> {
+                    isBeingIntook = false;
+                    isBeingQueued = false;
+                    motorOff();
+                });
+    }
+
+    /**
+     * @return Command that sets the motor speed to OuttakeSpeed.
+     */
+    public Command out() {
+        isBeingIntook = false;
+        isBeingQueued = false;
+        return runOnce(
+                () -> {
+                    isBeingIntook = false;
+                    isBeingQueued = false;
+                    motorOut();
+                });
+    }
+
+    /*public Command auto() {
+      while (ir.rightIR.getValue() < Constants.Intake.IRThreshold) {
+        return runOnce(() -> motorIn());
+      }
+      return runOnce(() -> motorOff());
+    }*/
+
+    /**
+     * @return Command that sets the motor speed to IntakeSpeed.
+     */
+    public Command in() {
+        return runOnce(
+                () -> {
+                    isBeingIntook = true;
+                    isBeingQueued = false;
+                    motorIn();
+                });
+    }
+
+    /**
+     * @return Command that slightly outtakes the note, used in conjucntion with the IR sensor to
+     *     clear the note from the flywheels
+     */
+    public Command queueNote() {
+        return runOnce(
+                () -> {
+                    isBeingIntook = false;
+                    isBeingQueued = true;
+                    motor.set(-0.05);
+                });
+    }
+
+    public Command stopNoteForShooting() {
+        return runOnce(
+                () -> {
+                    isBeingIntook = false;
+                    isBeingQueued = false;
+
+                    flywheel.on(500, 500).schedule();
+                    motorOff();
+                });
+    }
+
+    /**
+     * @return Command that sets the motor speed to UptakeSpeed.
+     */
+    public Command uptake() {
+        return runOnce(() -> motorUptake());
+    }
+
+    /**
+     * @return Command that toggles between intaking and not intaking. Does NOT take into account
+     *     other motor modes, ie. If this has been called, and the out Command is run, then when
+     *     this command is run again, the motor will just be turned off.
+     */
+    public Command toggleIntake() {
+        return runOnce(
+                () -> {
+                    if (state == true) {
+                        motorOff();
+                        state = false;
+                    } else {
+                        motorIn();
+                        state = true;
+                    }
+                });
+    }
+
+    @Override
+    public void periodic() {
+        // SmartDashboard.putNumber("IR", leftIR.getValue());
+    }
+
+    /**
+     * @return true if the note has been intook (used to signal intake off)
+     */
+    public boolean hasNote() {
+        // return ((ir.rightIR.getValue() > Constants.Intake.IRThreshold) || (ir.leftIR.getValue() >
+        // Constants.Intake.IRThreshold)) && isBeingIntook;
+        return closeToFlywheelSensor.getValue() > Constants.Intake.IRThreshold;
+    }
+
+    /**
+     * @return true if note is queued for shooting (used to clear note from flywheel)
+     */
+    public boolean noteIsQueued() {
+        return closeToFlywheelSensor.getValue() < Constants.Intake.IRThreshold;
+        // return ((ir.rightIR.getValue() < Constants.Intake.IRThreshold) && (ir.leftIR.getValue() <
+        // Constants.Intake.IRThreshold)) && isBeingQueued;
+    }
+
+    public boolean isBeingIntook() {
+        return isBeingIntook;
+    }
+
+    public boolean isBeingQueued() {
+        return isBeingQueued;
+    }
+
+    public void motorInWithRotations(double rotations) {
+        motor.setControl(
+                positionOut.withPosition(rotations)); // Not 100% Sure PositionOut uses rotations
+    }
 }
